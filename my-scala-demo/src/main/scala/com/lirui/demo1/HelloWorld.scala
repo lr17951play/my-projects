6e5971ce0cf85f1ab6ec5ad4d98fd6d44c57acf0
lr17951play <lr17951@qq.com>
2021-06-07 15:39:19 +0800

    scala Hello World

--- my-scala-demo/src/main/scala/com/lirui/demo1/HelloWorld.scala
+++ my-scala-demo/src/main/scala/com/lirui/demo1/HelloWorld.scala
@@ -5,5 +5,30 @@ object HelloWorld {
   def main(args: Array[String]): Unit = {
     println("Hello Scala")
     System.out.println("Hello From Java")
+
+    /** 常量 */
+    val a: Int = 10
+    /** 变量 */
+    var b = 20.555
+    b = a + b
+    printf("val: %d, var: %s", a, b)
+
+    val +-*/#@! = "zhangsan"
+    printf("特殊符号: %s", +-*/#@!)
+    println()
+    val `if` = "男"
+    printf("关键字: %s", `if`)
+
+    val name = "zhangsan"
+    val str =
+      s"""
+        |SELECT
+        |  *
+        |FROM
+        |  user
+        |WHERE
+        |  username = '$name'
+        |""".stripMargin
+    println(str)
   }
 }
