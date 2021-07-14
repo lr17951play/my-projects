d4c81cc3e01e3deb773be82076466be5897247cb
lr17951play <lr17951@qq.com>
2021-07-14 15:58:33 +0800

    Update HealthCurve.java
    health curve

--- my-java-demo/src/main/java/com/lirui/demo/algorithm/HealthCurve.java
+++ my-java-demo/src/main/java/com/lirui/demo/algorithm/HealthCurve.java
@@ -1,5 +1,8 @@
 package com.lirui.demo.algorithm;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 /**
  * @author Ryan
  */
@@ -30,17 +33,39 @@ public class HealthCurve {
      * */
     public static double sigmoid(double param) {
         double ey = Math.exp(10 - param);
-        double result = 1 - 1 / (1 + ey);
-        return result;
+        BigDecimal result = BigDecimal.ONE.subtract(
+                BigDecimal.ONE.divide(
+                        BigDecimal.ONE.add(BigDecimal.valueOf(ey)), 20, RoundingMode.HALF_UP)
+        );
+        return result.doubleValue();
+    }
+
+    /**
+     * @param factor (0, 1],参数切割的粒度，越接近于 0 则最终分数越高
+     * @return param (0, 20]
+     * */
+    private static double processParam(int day, int days, float factor) {
+        BigDecimal ratio = BigDecimal.valueOf(20).multiply(
+                BigDecimal.valueOf(day).divide(BigDecimal.valueOf(days), 5, RoundingMode.HALF_UP)
+        );
+        BigDecimal param = ratio.multiply(BigDecimal.valueOf(factor));
+        return param.doubleValue();
     }
 
     public static void main(String[] args) {
-        System.out.println(tanh(0));
-        System.out.println(arccot(-100));
-        System.out.println("----------------------- sigmoid ---------------------");
-        for (int i = 0; i < 21; i++) {
-            System.out.println(sigmoid(i));
+//        System.out.println(tanh(0));
+//        System.out.println(arccot(-100));
+
+        int year = 5;
+        float factor = 0.5f;
+        System.out.println("----------------------- sigmoid -----------------------");
+        double param;
+        int days = year * 365;
+        for (int day = 0; day <= days; day++) {
+            param = processParam(day, days, factor);
+            System.out.printf("param: %1.5f, result: %1.20f \n",param, sigmoid(param));
         }
 
     }
+
 }
